b4e77e94c2dd2033bf2e5a31a4aa8e4d311824e4
Can Cömertpay <cancomertpay@gmail.com>
2024-04-10 19:24:11 +0300

    fix terms undefined bug

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      
-    </div>
-  );
+  return redirect("/dictionary");
 }
