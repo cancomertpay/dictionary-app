af7a53ccd0ce17756cbca516599ab9b2c677c52f
Can Cömertpay <cancomertpay@gmail.com>
2024-04-10 20:49:49 +0300

    fix search bar border color bug

--- app/[searchTerm]/page.js
+++ app/[searchTerm]/page.js
@@ -2,10 +2,12 @@ import Dictionary from "@/components/main/dictionary/dictionary";
 import { fetchWord } from "@/lib/dictionary";
 
 async function SearchResult({ params }) {
+  if (!params.searchTerm) return null;
+
   const reverseModifyTerm = (text) => {
     return text?.replace(/-/g, " ");
   };
-  const data = await fetchWord(reverseModifyTerm(params?.searchTerm));
+  const data = await fetchWord(reverseModifyTerm(params.searchTerm));
   return (
     <div className="pb-14 overflow-hidden">
       {data?.map(
